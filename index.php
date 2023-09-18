a6bea1d025170349d67c59c5d1af82e82cc3253c
Angus <superangus@protonmail.com>
2023-09-18 21:58:03 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Piano Site</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="localise.js"></script>
+</head>
+<body>
+<div id="main-container">
+    <div id="left-side">
+
+    </div>
+    <div id="right-side">
+        <nav>
+            <ul>
+                <li>
+                    <div><a href="./pricing" data-lang="pricing"></a></div>
+                </li>
+                <li>
+                    <div><a href="./contact" data-lang="contact"></a></div>
+                </li>
+                <li>
+                    <div><a href="./about" data-lang="about"></a></div>
+                </li>
+                <li>
+                    <div><a href="./events" data-lang="events"></a></div>
+                </li>
+                <li>
+                    <div><a href="./lessons" data-lang="lessons"></a></div>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</div>
+</body>
+</html>
