ba5cb5e8cb0641cf187e6349c08be719e86bc89a
Angus <superangus@protonmail.com>
2023-09-29 14:25:23 +0200

    save

--- about.php
+++ about.php
@@ -1,1 +1,11 @@
-<?php
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Piano Site</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="localise.js"></script>
+</head>
+</html>
--- index.php
+++ index.php
@@ -11,27 +11,48 @@
 <body>
 <div id="main-container">
     <div id="left-side">
-
+        <div>
+            <h2>Suzanne Spanjaard</h2>
+            <p>
+                Piano docent in Veldhoven
+            </p>
+            <div style="padding-top:1rem;"></div>
+            <p>
+                De piano is een fantastisch instrument.
+                Je kunt er mee begeleiden, solospelen, je hart uitstorten, een feestje opvrolijken, je zinnen verzetten,
+                nieuwe werelden ontdekken....
+                Alleen, het gaat niet vanzelf.
+            </p>
+            <div style="padding-top:1rem;"></div>
+            <p>
+                Of je nu voor het eerst aan de piano zit of al jarenlang speelt
+                en een specifieke vaardigheid wilt verbeteren, ik help je graag
+                verder. Tijdens de lessen verdeel ik mijn aandacht over muzikale,
+                technische en harmonische aspecten van de stukken die je speelt.
+                Aan bod komen ook de verschillen in stijl en vorm en hoe je het
+                meest efficiënt kunt studeren. Improvisatie is een hulpmiddel dat
+                ik vaak gebruik om je zowel inzicht als handigheid te geven in
+                problemen die je tegenkomt. De instructies die ik geef zijn
+                toegespitst op wat jij wilt bereiken.
+            </p>
+            <div style="padding-top:1rem;"></div>
+            <p>
+                Waar ik naar streef is dat jij je eigen pianocoach wordt,
+                zodat je precies weet wat je wilt op de piano en hoe je dat
+                kunt bereiken. In de ruim 25 jaar dat ik pianodocente ben heb
+                ik aardig wat lesmateriaal ontwikkeld. Dit én mijn enthousiasme
+                en ervaring staan tot je beschikking bij jouw weg door de wereld
+                van piano en muziek.
+            </p>
+        </div>
     </div>
     <div id="right-side">
         <nav>
-            <ul>
-                <li>
-                    <div><a href="./pricing" data-lang="pricing"></a></div>
-                </li>
-                <li>
-                    <div><a href="./contact" data-lang="contact"></a></div>
-                </li>
-                <li>
-                    <div><a href="./about" data-lang="about"></a></div>
-                </li>
-                <li>
-                    <div><a href="./events" data-lang="events"></a></div>
-                </li>
-                <li>
-                    <div><a href="./lessons" data-lang="lessons"></a></div>
-                </li>
-            </ul>
+            <a href="./pricing" data-lang="pricing"></a>
+            <a href="./contact" data-lang="contact"></a>
+            <a href="./about" data-lang="about"></a>
+            <a href="./events" data-lang="events"></a>
+            <a href="./lesson" data-lang="lesson"></a>
         </nav>
     </div>
 </div>
